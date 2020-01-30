b426941b4fdc3e84da285404a18340099c667337
Mehmet Kerem Turkcan <mkt2126@columbia.edu>
2020-01-30 12:29:03 -0500

    Updating the neural network example

--- examples/multilayer_neural_network.py
+++ examples/multilayer_neural_network.py
@@ -6,15 +6,15 @@ in_neurons = C.add_cluster(784, LeakyIAF())
 # Create 32 Hodgkin-Huxley neurons and get their ID's
 middle_neurons = C.add_cluster(32, HodgkinHuxley())
 # Join nodes together via alpha synapses
-C.dense_connect_via(in_neurons, middle_neurons, AlphaSynapse())
+C.dense_connect_variable(in_neurons, middle_neurons, AlphaSynapse())
 # Create 10 more Hodgkin-Huxley neurons and get their ID's
 out_neurons = C.add_cluster(10, HodgkinHuxley())
 # Join nodes together via alpha synapses
-C.dense_connect_via(middle_neurons, out_neurons, AlphaSynapse())
+C.dense_connect_variable(middle_neurons, out_neurons, AlphaSynapse())
 # Create inputs for the first set of neurons
 input_list = []
 for i in in_neurons:
     input_list.append(InIStep(i, 40., 0.25, 0.50))
 # Simulate the circuit
-C.sim(1., 1e-4, input_list)
+C.sim(1., 1e-4, in_list=input_list)
 sim_results = C.collect_results() #Get simulation results
